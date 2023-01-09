acfd118afe150f9139d77d05d5e20e7cdb8300e9
Sophie <me@soopy.moe>
2023-01-09 22:21:03 +0800

    chore: canary

--- src/site/pages/canary.vue
+++ src/site/pages/canary.vue
@@ -13,7 +13,7 @@
 				Login to access your files and folders
 			</h2>-->
 			<div>
-				<i>Last signed and updated 2022-03-31T05:33:22,176194759+00:00</i>
+				<i>Last signed and updated 2023-01-09T14:13:52,183905132+00:00</i>
 				<ol>
 					<li>We have not recieved any kind of warrants to this site since its creation (2021-12-05)</li>
 					<li>Our database and this site has not been searched, seized or otherwise accessed by unauthorized entities (i.e. government departments, FBI and equivalent)</li>
@@ -23,6 +23,7 @@
 				<p>This message will be re-signed every 3 months. In addition, this document will also be re-signed every time it has been updated.</p>
 				<p>Every re-signature will be logged in the Past signatures section in the text version of this document, linked below.</p>
 				<b>If this is not updated within that timeframe, please assume something happened.</b>
+        <p><i>Word of warning: I'm pretty forgetful so I might miss timeframes by a bit. If I manage to miss the signing date by a maximum of 2 months, you can be absolutely sure something has happened.</i></p>
 				<hr>
 				<p>This document is and will be signed by Sophie Cheung &lt;<a href="mailto:me@soopy.moe">me@soopy.moe</a>&gt; <code>0xDB9BEC3A93B4B1DF</code></p>
 				<i>
